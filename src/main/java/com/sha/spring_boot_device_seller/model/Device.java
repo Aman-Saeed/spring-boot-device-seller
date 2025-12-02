071d11fa469f09f0eddb259c08987881c5e22d91
Amanullah Saeed <saeedarab29@gmail.com>
2025-12-02 12:13:12 +0430

    Initialized device class and deviceType enumerator

---
+++ src/main/java/com/sha/spring_boot_device_seller/model/Device.java
@@ -0,0 +1,34 @@
+package com.sha.spring_boot_device_seller.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "devices")
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = 1000)
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "create_time", nullable = false)
+    private LocalDateTime creatTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "device_type", nullable = false)
+    private DeviceType deviceType;
+
+}
