be984fbfacc9ad33dee988fb47c7e5cede5fd4ff
Amanullah Saeed <saeedarab29@gmail.com>
2025-12-09 15:05:01 +0430

    implemented authentication service

---
+++ src/main/java/com/sha/spring_boot_device_seller/service/AuthenticationServiceImpl.java
@@ -0,0 +1,35 @@
+package com.sha.spring_boot_device_seller.service;
+
+import com.sha.spring_boot_device_seller.model.User;
+import com.sha.spring_boot_device_seller.security.UserPrincipal;
+import com.sha.spring_boot_device_seller.security.jwt.JwtProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService{
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Override
+    public User signInAandReturnJWT(User signInRequest)
+    {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(signInRequest.getUsername()
+               , signInRequest.getPassword() ));
+
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        String jwt = jwtProvider.generateToken(userPrincipal);
+        signInRequest.setToken(jwt);
+
+        return signInRequest;
+    }
+
+}
