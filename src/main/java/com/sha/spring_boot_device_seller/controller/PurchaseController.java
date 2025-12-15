06fad7579c0d05251412647825209c9e577b0ef2
Amanullah Saeed <saeedarab29@gmail.com>
2025-12-15 10:23:16 +0430

    purchase controller added

---
+++ src/main/java/com/sha/spring_boot_device_seller/controller/PurchaseController.java
@@ -0,0 +1,32 @@
+package com.sha.spring_boot_device_seller.controller;
+
+
+import com.sha.spring_boot_device_seller.model.Purchase;
+import com.sha.spring_boot_device_seller.security.UserPrincipal;
+import com.sha.spring_boot_device_seller.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/purchase")
+public class PurchaseController {
+
+    @Autowired
+    private PurchaseService purchaseService;
+
+    @PostMapping
+    public ResponseEntity<?> savePurchase(@RequestBody Purchase purchase)
+    {
+       return new ResponseEntity<>(purchaseService.savePurchase(purchase), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllPurchasesOfUser(@AuthenticationPrincipal UserPrincipal userPrincipal)
+    {
+        return ResponseEntity.ok(purchaseService.findPurchaseItemOfUser(userPrincipal.getId()));
+    }
+
+}
