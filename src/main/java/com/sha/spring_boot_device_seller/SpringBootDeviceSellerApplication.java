39fd12d27c366b29149d199fe100bf97d2ca8400
Amanullah Saeed <saeedarab29@gmail.com>
2025-12-16 14:40:08 +0430

    minor changes

--- src/main/java/com/sha/spring_boot_device_seller/SpringBootDeviceSellerApplication.java
+++ src/main/java/com/sha/spring_boot_device_seller/SpringBootDeviceSellerApplication.java
@@ -2,10 +2,12 @@ package com.sha.spring_boot_device_seller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootDeviceSellerApplication {
 
     public PasswordEncoder passwordEncoder(){
